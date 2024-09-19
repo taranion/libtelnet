0ace38621bd42b7e8ad882098873dbacf663decc
Stefan Prelle <stefan@rpgframework.de>
2024-09-19 17:54:18 +0200

    Add charset

--- src/main/java/org/prelle/telnet/option/TelnetCharset.java
+++ src/main/java/org/prelle/telnet/option/TelnetCharset.java
@@ -4,14 +4,20 @@
 package org.prelle.telnet.option;
 
 import java.io.IOException;
+import java.lang.System.Logger;
 import java.lang.System.Logger.Level;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.HashMap;
 
 import org.prelle.telnet.CommunicationRole;
 import org.prelle.telnet.TelnetOption;
+import org.prelle.telnet.TelnetOptionListener;
 import org.prelle.telnet.TelnetOutputStream;
 import org.prelle.telnet.TelnetSocket;
 import org.prelle.telnet.TelnetSubnegotiationHandler;
+import org.prelle.telnet.option.TelnetEnvironmentOption.EnvironmentListener;
 
 /**
  * https://datatracker.ietf.org/doc/html/rfc2066
@@ -19,6 +25,16 @@ import org.prelle.telnet.TelnetSubnegotiationHandler;
  *
  */
 public class TelnetCharset extends TelnetSubnegotiationHandler {
+
+	protected final static Logger logger = System.getLogger("telnet.option.charset");
+
+	public static interface CharsetListener extends TelnetOptionListener {
+
+		public void telnetCharsetNegotiated(Charset charset);
+
+	}
+
+	public final static int CODE = 42;
 
 	private final static int REQUEST  = 1;
 	private final static int ACCEPTED = 2;
@@ -34,7 +50,40 @@ public class TelnetCharset extends TelnetSubnegotiationHandler {
 	 */
 	@Override
 	public void handleSubnegotiation(int code, int[] values, TelnetSocket origin, TelnetOutputStream out) {
-		logger.log(Level.WARNING, "TODO: Subnegotiate for CHARSET");
+		logger.log(Level.WARNING, "TODO: Subnegotiate for CHARSET: "+Arrays.toString(values));
+		int operation = values[0];
+		
+		if (operation==ACCEPTED) {
+			byte[] data = new byte[values.length-1];
+			for (int i=0; i<data.length; i++) {
+				data[i] = (byte)values[i+1];
+			}
+			String csName = new String(data);
+			System.err.println("TelnetCharset: Remote party agreed to use "+csName);
+			Charset charset = null;
+			if ("ISO 8859-15".equals(csName))
+				charset = StandardCharsets.ISO_8859_1;
+			else if ("ISO 8859-1".equals(csName))
+				charset = StandardCharsets.ISO_8859_1;
+			else
+				charset = Charset.forName(csName);
+			
+			try {
+				CharsetListener listener = origin.getOptionListener(code);
+				if (listener!=null) {
+					listener.telnetCharsetNegotiated(charset);
+				} else {
+					logger.log(Level.TRACE, "No CharsetListener");
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else if (operation==REJECTED) {
+			logger.log(Level.WARNING, "The remote party rejected our charset suggestions");
+			System.err.println("TelnetCharset: Remote party rejected charsets");
+		}
+		
 	}
 
 	//-----------------------------------------------------------------
@@ -43,7 +92,28 @@ public class TelnetCharset extends TelnetSubnegotiationHandler {
 	 * @return TRUE when answers to a subnegotiation are expected
 	 */
 	public boolean initializeAs(TelnetOption option, CommunicationRole role, TelnetSocket origin, TelnetOutputStream out) {
+		logger.log(Level.WARNING, "TODO: initializeAs "+role);
 		if (role==CommunicationRole.SERVER) {
+			logger.log(Level.DEBUG, "Ask remote party to send environment");
+			try {
+				origin.setOptionData(CODE, new HashMap<String,String>());
+				byte[] charsetData = "UTF-8 CP437 ASCII".getBytes(StandardCharsets.US_ASCII);
+				byte[] send = new byte[charsetData.length+7];
+				send[0] = (byte)IAC;
+				send[1] = (byte)SB;
+				send[2] = (byte)CODE;
+				send[3] = (byte)REQUEST;
+				send[4] = (byte)32;
+				System.arraycopy(charsetData, 0, send, 5, charsetData.length);
+				send[5+charsetData.length] = (byte)IAC;
+				send[6+charsetData.length] = (byte)SE;
+				out.writeCommand(send);
+				out.flush();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return true;
 		} else {
 			logger.log(Level.WARNING, "Acting as PROVIDER not implemented");
 		}
